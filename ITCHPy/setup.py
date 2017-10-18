c1809752efb4fe98c51eee7051f4d144480a9ee9
Andreas Tarnowsky <atarnows@welfenlab.de>
2017-10-18 10:09:56 +0200

    Fix build dependencies for ITCHPy

--- ITCHPy/setup.py
+++ ITCHPy/setup.py
@@ -1,15 +1,12 @@
 from distutils.core import setup, Extension
 
-SCRATCHPy = Extension('ITCHPy',
-                    #include_dirs = [
-                    #'/usr/include/python3.4m',
-                    #'/usr/include/arm-linux-gnueabihf/qt5/', '/usr/include/arm-linux-gnueabihf/qt5/QtCore/', '/usr/include/arm-linux-gnueabihf/qt5/QtGui'],
-                    include_dirs = ['/usr/include/python3.6m', '/usr/include/qt/', '/usr/include/qt/QtCore', '/usr/include/qt/QtGui','../libITCHy/', '../../SCRATCHy/libSCRATCHy/'],
-                    sources = [
-                    'itchpy.cpp'],
-                    library_dirs=[ '../build-libITCHy/' ],
-                    libraries = ['boost_python3', 'ITCHy'],
-                    extra_compile_args=['-std=c++11'])
+SCRATCHPy = Extension('ITCHPy',                    
+                    include_dirs = ['/usr/include/python3.6m', '/usr/include/qt/', '/usr/include/qt/QtCore', '/usr/include/qt/QtGui','/usr/include/itchy', '/usr/include/scratchy'],
+                    sources = ['itchpy.cpp'],
+                    library_dirs=['/usr/lib/'],
+                    libraries = ['boost_python3', 'ITCHy', 'Qt5Core', 'Qt5Gui'],
+                    extra_compile_args=['-std=c++11'],
+                    extra_link_args=['-Wl,--no-allow-shlib-undefined'])
 
 setup (name = 'ITCHPy Interface',
        version = '.1',
